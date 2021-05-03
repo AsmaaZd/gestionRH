6023f56ea9cca3434306f22f01603c1112434fb3
AsmaaZd <ziadi.asmaa.Ginfo@gmail.com>
2021-05-03 18:16:40 +0200

    design update

--- src/Form/CompetenceType.php
+++ src/Form/CompetenceType.php
@@ -6,13 +6,22 @@ use App\Entity\Competence;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CompetenceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('competence')
+            ->add('competence', TextType::class, [
+                'required' => false,
+                'label' => 'Compétence',
+                'attr' => [
+                    'placeholder' => "Exp:PHP..",
+                    'class' => "form-control"
+                ],
+
+            ])
             // ->add('profils')
         ;
     }
