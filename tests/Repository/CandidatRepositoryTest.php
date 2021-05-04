355e7d6d59ca9cfee4560f1f32cdd5e982fbc0c5
AsmaaZd <ziadi.asmaa.Ginfo@gmail.com>
2021-05-04 15:26:05 +0200

    phpUnit( db test with AliceFakerPhp )

--- tests/Repository/CandidatRepositoryTest.php
+++ tests/Repository/CandidatRepositoryTest.php
@@ -3,19 +3,28 @@
 namespace App\Tests\Repository;
 
 
+
 use App\DataFixtures\CandidatFixtures;
 use App\Repository\CandidatRepository;
 use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class CandidatRepositoryTest extends KernelTestCase{
+class CandidatRepositoryTest extends KernelTestCase
+{
 
     use FixturesTrait;
 
-    public function testCandidat(){
+    public function testCandidat()
+    {
         self::bootKernel();
-        $this->loadFixtures([CandidatFixtures::class]);
-        $candidats= self::$container->get(CandidatRepository::class)->count([]);
-        $this->assertEquals(10,$candidats);
+        // $this->loadFixtures([CandidatFixtures::class]);
+        $loader = new \Nelmio\Alice\Loader\NativeLoader();
+        $candidats = $loader->loadFile(__DIR__ . '/CandidatRepositoryTestFixtures.yaml');
+        // $candidats = $this->loadFixtureFiles([
+        //     __DIR__ . '/CandidatRepositoryTestFixtures.yaml'
+        // ]);
+        // $candidats['candidat1'];
+        $candidats = self::$container->get(CandidatRepository::class)->count([]);
+        $this->assertEquals(10, $candidats);
     }
-} 
+}
